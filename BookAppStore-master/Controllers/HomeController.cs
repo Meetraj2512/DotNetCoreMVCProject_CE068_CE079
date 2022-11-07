a1ea589f13597fdbb8ca45d7e8ba27c5bc112283
Meetraj2512 <85874043+Meetraj2512@users.noreply.github.com>
2022-11-07 12:32:51 +0530

    First Commit

---
+++ BookAppStore-master/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using BookAppStore.Models;
+using BookAppStore.Repository;
+
+namespace BookAppStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly BookRepository _bookRepository;
+
+        public HomeController(ILogger<HomeController> logger, BookRepository bookRepository)
+        {
+            _logger = logger;
+            _bookRepository = bookRepository;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var data = await _bookRepository.GetAllBookAsync();
+            return View(data);
+        }
+
+        [Route("about-us")]
+        public IActionResult AboutUs()
+        {
+            return View();
+        }
+
+        [Route("privacy")]
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
